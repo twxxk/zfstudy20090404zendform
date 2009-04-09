0c261376798e827b6c5eb6dab3692284678344f4
twk <twk@nonn-et-twk.net>
2009-04-09 15:22:22 +0900

    include_path

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
-require_once 'Zend/Loader.php';
-
 set_include_path(
 	'application' . DIRECTORY_SEPARATOR . 'library'
 	. PATH_SEPARATOR . get_include_path());
+
+require_once 'Zend/Loader.php';
 
 Zend_Loader::registerAutoload();
 Zend_Controller_Front::getInstance()->throwExceptions(true);
